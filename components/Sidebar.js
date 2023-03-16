b0169a383bec95d332f7927b1f37624dd8953b74
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-03-16 16:40:25 +0530

    final commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,54 +2,56 @@ import { auth } from "@/firebaseConfig";
 import { ArrowLeftIcon } from "@chakra-ui/icons";
 import { Avatar, Button, Flex, IconButton, Text } from "@chakra-ui/react";
 import { signOut } from "firebase/auth";
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollection } from 'react-firebase-hooks/firestore';
-import {collection,addDoc} from 'firebase/firestore'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "@/firebaseConfig";
 import getOtherEmail from "@/utils/getOtherEmail";
 import { useRouter } from "next/router";
 
 function Sidebar() {
-    const [user] = useAuthState(auth);
-    const [snapshot, loading, error] = useCollection(collection(db,"chats"));
-    const chats = snapshot?.docs.map(doc=>({id:doc.id, ...doc.data()}));
-    const router = useRouter();
-    const redirect=(id)=>{
-        router.push(`/chat/${id}`);
+  const [user] = useAuthState(auth);
+  const [snapshot, loading, error] = useCollection(collection(db, "chats"));
+  const chats = snapshot?.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  const router = useRouter();
+  const redirect = (id) => {
+    router.push(`/chat/${id}`);
+  };
+  const chatExists = (email) =>
+    chats?.find(
+      (chat) => chat.users.includes(user.email) && chat.users.includes(email)
+    );
+  const newChat = async () => {
+    const input = prompt("Enter Email of Chat recipient");
+    if (!chatExists(input) && input != user.email) {
+      await addDoc(collection(db, "chats"), { users: [user.email, input] });
     }
-    const chatExists = email =>chats?.find(chat=>(chat.users.includes(user.email)&& chat.users.includes(email )))
-    const newChat= async ()=>{
-        const input = prompt("Enter Email of Chat recipient");
-        if(!chatExists(input)&& input!=user.email)
-        {
-            await addDoc(collection(db,"chats"),{users:[user.email,input]})
-
-        }
-        
-    }
-    const chatList=()=>{
-        return(
-            chats?.filter(chat=>chat.users.includes(user.email))
-            .map(chat=>
-                <Flex key={Math.random()} p={3} align='center' _hover={{bg:'gray.100',cursor:'pointer'}} onClick={()=>redirect(chat.id)}>
-                <Avatar src='' marginEnd={3}/>
-                <Text>{getOtherEmail(chat.users,user)}</Text>
-              </Flex>
-            ) 
-        )
-    }
+  };
+  const chatList = () => {
+    return chats
+      ?.filter((chat) => chat.users.includes(user.email))
+      .map((chat) => (
+        <Flex
+          key={Math.random()}
+          p={3}
+          align="center"
+          _hover={{ bg: "gray.100", cursor: "pointer" }}
+          onClick={() => redirect(chat.id)}
+        >
+          <Avatar src="" marginEnd={3} />
+          <Text>{getOtherEmail(chat.users, user)}</Text>
+        </Flex>
+      ));
+  };
   return (
     <Flex
-      //   bg='blue.100'
       w="300px"
       h="100%"
       borderEnd="1px solid"
       borderColor="gray.200"
       direction="column"
-    //   position='fixed'
     >
       <Flex
-        // bg='red.100'
         h="81px"
         w="100%"
         align="center"
@@ -60,19 +62,24 @@ function Sidebar() {
           <Avatar src={user.photoURL} marginEnd={3} />
           <Text>{user.displayName}</Text>
         </Flex>
-        <IconButton size="sm" isRound icon={<ArrowLeftIcon />} onClick={()=>signOut(auth)}/>
+        <IconButton
+          size="sm"
+          isRound
+          icon={<ArrowLeftIcon />}
+          onClick={() => signOut(auth)}
+        />
       </Flex>
-      <Button m={5} p={4} onClick={()=>newChat()}>
+      <Button m={5} p={4} onClick={() => newChat()}>
         New Chat
       </Button>
-      <Flex overflowX='scroll' direction='column' sx={{scrollbarWidth:'none'}} flex={1} >
-
-     { chatList()}
-     
-
-  
+      <Flex
+        overflowX="scroll"
+        direction="column"
+        sx={{ scrollbarWidth: "none" }}
+        flex={1}
+      >
+        {chatList()}
       </Flex>
-     
     </Flex>
   );
 }
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,113 +1,130 @@
-import React, { useEffect, useState , useRef } from 'react'
-import Sidebar from '@/components/Sidebar'
-import { Avatar, Button, Flex, FormControl, Heading, Input, Text } from '@chakra-ui/react'
-import Head from 'next/head'
+import React, { useEffect, useState, useRef } from "react";
+import Sidebar from "@/components/Sidebar";
+import {
+  Avatar,
+  Button,
+  Flex,
+  FormControl,
+  Heading,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import Head from "next/head";
 import { useRouter } from "next/router";
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { addDoc, collection, doc, orderBy, query, serverTimestamp} from 'firebase/firestore';
-import { db } from '@/firebaseConfig';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '@/firebaseConfig';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
-import getOtherEmail from '@/utils/getOtherEmail';
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebaseConfig";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import getOtherEmail from "@/utils/getOtherEmail";
 
-const Topbar =({email})=>{
-  return(
-    <Flex bg='gray.100'
-    h='81px' w='100%'
-    align='center'  p={5}>
-     
-      <Avatar src='' marginEnd={3}/>
-      <Heading size='lg'>{email}</Heading>
+const Topbar = ({ email }) => {
+  return (
+    <Flex bg="gray.100" h="81px" w="100%" align="center" p={5}>
+      <Avatar src="" marginEnd={3} />
+      <Heading size="lg">{email}</Heading>
     </Flex>
-  )
- 
-}
-const Bottombar=({id,user})=>
-{
-  const [input,setInput]=useState("");
-  const sendMessage=async (e)=>{
-      e.preventDefault();
-      await addDoc(collection(db,`chats/${id}/messages`),{
-        text:input,
-        sender : user.email,
-        timestamp :serverTimestamp()
-
-      })
-      setInput("");
-  }
-  return(
-    <FormControl  p={3}
-  
-    onSubmit={sendMessage}
-    as= "form">
-      <Input placeholder='Type a message..' autoComplete='off' onChange={e=>setInput(e.target.value)} value={input}/>
-      <Button type="submit" hidden  >Submit</Button>
+  );
+};
+const Bottombar = ({ id, user }) => {
+  const [input, setInput] = useState("");
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    await addDoc(collection(db, `chats/${id}/messages`), {
+      text: input,
+      sender: user.email,
+      timestamp: serverTimestamp(),
+    });
+    setInput("");
+  };
+  return (
+    <FormControl p={3} onSubmit={sendMessage} as="form">
+      <Input
+        placeholder="Type a message.."
+        autoComplete="off"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <Button type="submit" hidden>
+        Submit
+      </Button>
     </FormControl>
-  )
-}
+  );
+};
 
-function  Chat() {
+function Chat() {
   const [user] = useAuthState(auth);
-  const router =useRouter();
-  const {id} = router.query;
-  const q = query(collection(db,`chats/${id}/messages`),orderBy("timestamp"));
-  const [messages] =useCollectionData(q);
-  const [chat] = useDocumentData(doc(db,"chats",id));
-  const bottomOfChat=useRef();
-  
-  const getMessages=()=>
-  messages?.map(msg=>{
+  const router = useRouter();
+  const { id } = router.query;
+  const q = query(collection(db, `chats/${id}/messages`), orderBy("timestamp"));
+  const [messages] = useCollectionData(q);
+  const [chat] = useDocumentData(doc(db, "chats", id));
+  const bottomOfChat = useRef();
 
-    const sender = msg.sender === user.email
-    return(
-      <Flex key={Math.random()} alignSelf={sender ? 'flex-start':'flex-end'} bg={sender ? 'blue.100':'green.100'} direction='column' w='fit-content' minWidth='100px' borderRadius='lg' p={3} m={1}>
-      <Text>{msg.text}</Text>
-    </Flex>
-    )
-  }
-  )
-  const scroll=()=>
-  {
+  const getMessages = () =>
+    messages?.map((msg) => {
+      const sender = msg.sender === user.email;
+      return (
+        <Flex
+          key={Math.random()}
+          alignSelf={sender ? "flex-start" : "flex-end"}
+          bg={sender ? "blue.100" : "green.100"}
+          direction="column"
+          w="fit-content"
+          minWidth="100px"
+          borderRadius="lg"
+          p={3}
+          m={1}
+        >
+          <Text>{msg.text}</Text>
+        </Flex>
+      );
+    });
+  const scroll = () => {
     setTimeout(
       bottomOfChat.current.scrollIntoView({
-      behavior: "smooth",
-      block: 'start',
-    }), 100)
-
-  }
-  useEffect(() =>
-  {
+        behavior: "smooth",
+        block: "start",
+      }),
+      100
+    );
+  };
+  useEffect(() => {
     scroll();
-  }
-  
-, [messages])
-
+  }, [messages]);
 
   return (
-    
-    <Flex h='100vh' >
+    <Flex h="100vh">
       <Head>
         <title>Chat App</title>
       </Head>
 
-    <Sidebar/>
-    <Flex flex={1} direction='column' >
-    <Topbar email={getOtherEmail(chat?.users,user)}/>
-    <Flex flex={1} direction='column' pt={4} mx={5} overscrollX="scroll" sx={{scrollbarWidth:"none"}}>
-    
-      {getMessages()}
-      <div ref={bottomOfChat}></div>
-   
-    
+      <Sidebar />
+      <Flex flex={1} direction="column">
+        <Topbar email={getOtherEmail(chat?.users, user)} />
+        <Flex
+          flex={1}
+          direction="column"
+          pt={4}
+          mx={5}
+          overscrollX="scroll"
+          sx={{ scrollbarWidth: "none" }}
+        >
+          {getMessages()}
+          <div ref={bottomOfChat}></div>
+        </Flex>
+        <Bottombar id={id} user={user} />
+      </Flex>
     </Flex>
-    <Bottombar id={id} user={user}/>
-
-    </Flex>
-    
-    
-    </Flex>
-  )
+  );
 }
 
-export default  Chat
+export default Chat;
